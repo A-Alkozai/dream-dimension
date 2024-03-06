670a3dc163310b7a4f349b21bf953a13d1e3ed38
Hunte Andrew (2023) ZMAC611 <andrew.hunte.2023@live.rhul.ac.uk>
2024-03-06 22:40:55 +0000

    Update stamina.py now incorporated a dash button which will be used to allow our sprite to dash 3 times until stamina is fully regained, there is also a reset dash count just to play around with lol

--- stamina.py
+++ stamina.py
@@ -1,80 +1,65 @@
-import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
+# Global variables
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 150
+STAMINA_MAX = 100
+STAMINA_REGEN_RATE = 1.0  # Increase regeneration rate
+STAMINA_DASH_COST = 30
+STAMINA_DASH_COUNT = 3
 
-# Class to represent a StaminaBar
-class StaminaBar:
-    def __init__(self, max_stamina, position, width, height, color, decrease_rate):
-        self.max_stamina = max_stamina
-        self.current_stamina = max_stamina  
-        self.position = position  
-        self.width = width  
-        self.height = height  
-        self.color = color  
-        self.decrease_rate = decrease_rate  
+stamina = STAMINA_MAX
+dashes_left = STAMINA_DASH_COUNT
 
-        # Create a timer to decrease stamina
-        self.timer = simplegui.create_timer(1000, self.decrease_stamina)
+# Timer handler for stamina regeneration
+def regen_stamina():
+    global stamina
+    if stamina < STAMINA_MAX:
+        stamina += STAMINA_REGEN_RATE
+        if stamina > STAMINA_MAX:
+            stamina = STAMINA_MAX
 
-        # Flag to track if any movement key is pressed
-        self.is_movement_key_pressed = False
+# Timer for stamina regeneration
+timer = simplegui.create_timer(300, regen_stamina)  # Decrease timer interval for faster regeneration
 
-    # Method to draw the stamina bar on the canvas
-    def draw(self, canvas):
-        # Calculate the width of the stamina bar based on current stamina
-        bar_width = self.width * (self.current_stamina / self.max_stamina)
-        # Draw the stamina bar on the canvas
-        canvas.draw_polygon([(self.position[0], self.position[1]),
-                             (self.position[0] + bar_width, self.position[1]),
-                             (self.position[0] + bar_width, self.position[1] + self.height),
-                             (self.position[0], self.position[1] + self.height)],
-                            2, self.color, self.color)
+# Handler for dash action
+def dash():
+    global stamina, dashes_left
+    if dashes_left > 0 and stamina >= STAMINA_DASH_COST:
+        stamina -= STAMINA_DASH_COST
+        dashes_left -= 1
 
-    # Method to start the timer for decreasing stamina
-    def start_timer(self):
-        self.timer.start()
+# Handler for resetting dashes
+def reset_dashes():
+    global dashes_left
+    dashes_left = STAMINA_DASH_COUNT
 
-    # Method to stop the timer for decreasing stamina
-    def stop_timer(self):
-        self.timer.stop()
-
-    # Method to decrease the stamina
-    def decrease_stamina(self):
-        if self.is_movement_key_pressed:
-            self.current_stamina = max(self.current_stamina - self.decrease_rate, 0)
-
-    # Method to handle key down events
-    def key_down(self, key):
-        # Start the timer if a movement key is pressed
-        if key in (simplegui.KEY_MAP["w"], simplegui.KEY_MAP["a"], simplegui.KEY_MAP["s"], simplegui.KEY_MAP["d"]):
-            self.is_movement_key_pressed = True
-            self.start_timer()
-
-    # Method to handle key up events
-    def key_up(self, key):
-        # Stop the timer if no movement keys are pressed
-        if key in (simplegui.KEY_MAP["w"], simplegui.KEY_MAP["a"], simplegui.KEY_MAP["s"], simplegui.KEY_MAP["d"]):
-            self.is_movement_key_pressed = False
-            self.stop_timer()
-
-
-# Draw handler function for the frame
+# Handler for drawing on the canvas
 def draw(canvas):
-    global stamina_bar
+    # Draw background
+    canvas.draw_polygon([(0, 0), (CANVAS_WIDTH, 0), (CANVAS_WIDTH, CANVAS_HEIGHT), (0, CANVAS_HEIGHT)], 10, 'Black')
+    
+    # Draw stamina bar
+    bar_width = stamina / STAMINA_MAX * (CANVAS_WIDTH - 20)
+    canvas.draw_line((10, 100), (bar_width + 10, 100), 10, "Green")
+    canvas.draw_line((bar_width + 10, 100), (CANVAS_WIDTH - 10, 100), 10, "Red")
+    canvas.draw_text("Stamina:", [10, 125], 20, "White")
+    canvas.draw_text(str(round(stamina)), [100, 125], 20, "White")
+    
+    # Draw dashes left
+    canvas.draw_text("Dashes left: " + str(dashes_left), [10, 144], 20, "White")
 
-    # Draw the stamina bar on the canvas
-    stamina_bar.draw(canvas)
+# Create frame and register event handlers
+frame = simplegui.create_frame("Stamina Bar", CANVAS_WIDTH, CANVAS_HEIGHT)
 
-
-# Create a frame
-frame = simplegui.create_frame("Stamina Bar Example", 300, 200)
 frame.set_draw_handler(draw)
+frame.add_button("Dash", dash, 100)
 
-# Initialize stamina bar with maximum stamina of 100
-stamina_bar = StaminaBar(100, (50, 50), 200, 20, "blue", decrease_rate=1)
+frame.add_button("Reset Dashes", reset_dashes, 100)
 
-# Set key down and key up handlers
-frame.set_keydown_handler(stamina_bar.key_down)
-frame.set_keyup_handler(stamina_bar.key_up)
-
-# Start the frame
+# Start timers
+timer.start()
 frame.start()
