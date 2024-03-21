2b9254c4b63e97776e8bcdc7b86fe3821f846d03
Hunte Andrew (2023) ZMAC611 <andrew.hunte.2023@live.rhul.ac.uk>
2024-03-21 09:26:00 +0000

    Update player.py mana is now within player class with all its functionalties

--- player.py
+++ player.py
@@ -3,10 +3,14 @@ from vector import Vector
 from state import State
 
 class Player(State):
-    def __init__(self, **kwargs):
+    def __init__(self, mana_max=100, mana_recharge_rate=1, **kwargs):
         super().__init__(**kwargs)
         self.points = 0
-        self.lives = 3  # Initialize lives(we can change this value if need be)
+        self.lives = 3
+        self.mana_max = mana_max
+        self.mana_current = mana_max
+        self.mana_recharge_rate = mana_recharge_rate
+        self.mana_recharge_timer = simplegui.create_timer(1000, self.recharge_mana)
 
     def state_update(self):
         pass
@@ -21,3 +25,12 @@ class Player(State):
     def key_up(self, key):
         if key == simplegui.KEY_MAP["a"]: self.LEFT = False
         if key == simplegui.KEY_MAP["d"]: self.RIGHT = False
+
+    def start_mana_recharge(self):
+        self.mana_recharge_timer.start()
+
+    def stop_mana_recharge(self):
+        self.mana_recharge_timer.stop()
+
+    def recharge_mana(self):
+        self.mana_current = min(self.mana_current + self.mana_recharge_rate, self.mana_max)
